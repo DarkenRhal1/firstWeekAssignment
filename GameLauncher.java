f5d5bcf4ceb3b1b0fa2738bc2759fb6757cca543
DarkenRhal1 <144154657+DarkenRhal1@users.noreply.github.com>
2023-09-10 18:48:28 -0400

    questionn2

---
+++ GameLauncher.java
@@ -0,0 +1,9 @@
+package org.example.questionn;
+
+public class GameLauncher {
+
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
